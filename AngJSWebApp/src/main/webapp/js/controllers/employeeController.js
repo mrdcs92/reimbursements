41533aca4ba6356c4315a8fa19eec383231e1827
Dylan Smith <mrdcs92@gmail.com>
2018-09-23 21:16:55 -0400

    employee can view all reimbursements

--- AngJSWebApp/src/main/webapp/js/controllers/employeeController.js
+++ AngJSWebApp/src/main/webapp/js/controllers/employeeController.js
@@ -10,11 +10,14 @@
     function employeeController($routeParams, $q, $http){
 
         var vm = this;
+        vm.isBusy = false;
 
         vm.nameParam = $routeParams.username;
         let idParam = $routeParams.userid;
+        let reimbursements;
 
         vm.getReimbursements = function (){
+            vm.isBusy = true;
             let data = $.param({employeeId: idParam});
 
             let config = {
@@ -25,10 +28,19 @@
 
             $http.post('/AngJSWebApp/getReimbursements.do', data, config)
                 .then(function (response){
-                    let data = response.data;
-                    console.log(data);   
+                    reimbursements = response.data;
+                    for (let i = 0; i < reimbursements.length; i++) {
+                        for (let prop in reimbursements[i]){
+                            if (reimbursements[i][prop] == null) {
+                                reimbursements[i][prop] = "N/A";
+                            }
+                        }                    
+                    }
+                    vm.isBusy = false;
+                    vm.reimbursements = reimbursements;  
                 },
                  function(response){
+                     vm.isBusy = false;
                      console.log(response);
                  });
         }
